cf182f83f227b39c6acc1b3a90905f7941b7d4ef
Nikita Menkovich <librarian@nebius.com>
2024-02-12 17:12:24 +0100

    fix

--- .github/scripts/vm-watcher.py
+++ .github/scripts/vm-watcher.py
@@ -1,27 +1,70 @@
-from yandex.cloud.compute.v1.instance_service_pb2_grpc import InstanceServiceStub
-from yandex.cloud.compute.v1.instance_service_pb2 import ListInstancesRequest,DeleteInstanceRequest
-from yandexcloud import SDK,RetryInterceptor
-import os
 import grpc
+import json
+import logging
+import argparse
 from datetime import datetime, timedelta
+from yandexcloud import SDK, RetryInterceptor
+from yandex.cloud.compute.v1.instance_service_pb2_grpc import InstanceServiceStub
+from yandex.cloud.compute.v1.instance_service_pb2 import (
+    ListInstancesRequest,
+    DeleteInstanceRequest,
+)
 
-CACHE_VM_ID="dp7329odurnhplpf5ff0"
-threshold_time = datetime.now() - timedelta(hours=24)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s"
+)
 
-interceptor = RetryInterceptor(max_retry_count=5, retriable_codes=[grpc.StatusCode.UNAVAILABLE])
+CACHE_VM_ID = "dp7329odurnhplpf5ff0"
 
-sdk = SDK(iam_token=os.getenv("IAM_TOKEN"), endpoint="api.ai.nebius.cloud", interceptor=interceptor)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--service-account-key",
+        required=True,
+        help="Path to the service account key file",
+    )
+    parser.add_argument(
+        "--folder-id",
+        required=True,
+        help="The ID of the folder to list instances in",
+        default="bjeuq5o166dq4ukv3eec",
+    )
+    parser.add_argument(
+        "--ttl", required=True, help="The TTL for the VMs", default=24, type=int
+    )
+    parser.add_argument("--apply", action="store_true", help="Apply the changes")
 
-client = sdk.client(InstanceServiceStub)
-response = client.List(ListInstancesRequest(folder_id=os.getenv("FOLDER_ID")))
+    args = parser.parse_args()
 
-for vm in response.instances:
-    if vm.id == CACHE_VM_ID:
-        continue
+    treshold_time = datetime.now() - timedelta(hours=args.ttl)
 
-    creation_time = vm.created_at.ToDatetime()
-    if creation_time < threshold_time:
-        print(f"VM {vm.id} is older than 24 hours, deleting it")
-        client.Delete(DeleteInstanceRequest(instance_id=vm.id))
-    else:
-        print(f"VM {vm.id} is younger than 24 hours, keeping it, created at {creation_time}")
+    interceptor = RetryInterceptor(
+        max_retry_count=5, retriable_codes=[grpc.StatusCode.UNAVAILABLE]
+    )
+
+    with open(args.service_account_key, "r") as fp:
+        sdk = SDK(
+            service_account_key=json.load(fp),
+            endpoint="api.ai.nebius.cloud",
+            interceptor=interceptor,
+        )
+
+    client = sdk.client(InstanceServiceStub)
+    response = client.List(ListInstancesRequest(folder_id=args.folder_id))
+
+    for vm in response.instances:
+        if vm.id == CACHE_VM_ID:
+            logging.info(f"Skipping VM {vm.id} as it is a cache VM")
+            continue
+
+        creation_time = vm.created_at.ToDatetime()
+        if creation_time < treshold_time:
+            logging.info(
+                f"VM {vm.id} is older than 24 hours, deleting it, created at {creation_time}"
+            )
+            if args.apply:
+                client.Delete(DeleteInstanceRequest(instance_id=vm.id))
+        else:
+            logging.info(
+                f"VM {vm.id} is younger than 24 hours, keeping it, created at {creation_time}"
+            )
