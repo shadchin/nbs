08afa3087979b9dea008ff87de4a96dd68a977c4
Evgeniy Kozev <evgeniy-kozev@yandex-team.ru>
2024-05-14 14:45:18 +0300

    small trace processor refactoring

---
+++ cloud/storage/core/libs/diagnostics/trace_processor.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "public.h"
+
+#include <cloud/storage/core/libs/common/startable.h>
+
+namespace NLWTrace {
+class TManager;
+}   // namespace NLWTrace
+
+namespace NCloud {
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct ITraceProcessor
+    : public IStartable
+{
+    virtual ~ITraceProcessor() = default;
+
+    virtual void ForEach(std::function<void(ITraceReaderPtr)> fn) = 0;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+ITraceProcessorPtr CreateTraceProcessor(
+    ITimerPtr timer,
+    ISchedulerPtr scheduler,
+    ILoggingServicePtr logging,
+    TString componentName,
+    NLWTrace::TManager& lwManager,
+    TVector<ITraceReaderPtr> keepers);
+
+ITraceProcessorPtr CreateTraceProcessorStub();
+
+}   // namespace NCloud
